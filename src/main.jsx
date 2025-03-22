faba63531d2d2d35f88d83cdf6eecc205094f15b
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2025-03-22 00:22:22 -0300

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./main.scss";
 import ContextProviderCart from "./contexts/contextCart";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <ContextProviderCart>
                 <App />
             </ContextProviderCart>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 );
